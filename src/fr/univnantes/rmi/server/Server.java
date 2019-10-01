2bb9be40bca74a922e44b608b2bb581e7330789a
Henri <bouvet.henri@gmail.com>
2019-10-01 14:11:59 +0200

    Correspondance UML

--- src/fr/univnantes/rmi/server/Server.java
+++ src/fr/univnantes/rmi/server/Server.java
@@ -1,7 +1,13 @@
 package fr.univnantes.rmi.server;
 
+import java.io.Serializable;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class Server implements Remote {
+public class Server extends UnicastRemoteObject implements Remote {
     private Game game;
+
+    protected Server() throws RemoteException {
+    }
 }
