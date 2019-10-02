252742aa826c022f728c503d803975cb60d0c509
Raphael Page <page.raf@gmail.com>
2019-10-02 21:57:37 +0200

    Merge branch 'master' of D:\Dev\workspace\RMIPresident with conflicts.

--- src/fr/univnantes/rmi/server/Server.java
+++
@@ -1,6 +0,0 @@
-package fr.univnantes.rmi.server;
-
-import java.rmi.Remote;
-
-public interface Server extends  Remote {
-}
--- src/fr/univnantes/rmi/server/ServerImpl.java
+++
@@ -1,9 +0,0 @@
-package fr.univnantes.rmi.server;
-
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-
-public class ServerImpl extends UnicastRemoteObject implements Server {
-    protected ServerImpl() throws RemoteException {
-    }
-}
